0504db0f29d36a5f880be5d8c4d149d31b59ff2f
kontarkantor <80453380+kontarkantor@users.noreply.github.com>
2022-03-20 21:02:14 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -19,7 +19,7 @@ Config.Factions = {'police', 'ambulance'}
 
 Config.AdminGroups = {'admin', 'superadmin'}
 Config.ShoowRoomCoords = {x = -46.1, y = -1096.43, z = 25.71, h = 230.0}
-Config.ShoowRoomCam = {x = -45.54, y = -1100.37, z = 26.42, xr = 0.0, yr = 0.0, zr = 0.0, f = 280}
+Config.ShoowRoomCam = {x = -45.54, y = -1100.37, z = 27.42, xr = 0.0, yr = 0.0, zr = 0.0, f = 280}
 Config.Webhook = "WEBHOOK"
 Config.UNKpng = "http://clipart-library.com/images/8TGb9bdzc.png"
 
